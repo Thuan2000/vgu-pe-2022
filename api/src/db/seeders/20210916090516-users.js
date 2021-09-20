b8c8023cf4b0fefc90573d929ec83792a62bf77e
fatwaanugera21 <fatwaanugera21@gmail.com>
2021-09-20 18:58:59 +0800

    Adding seed account

--- api/src/db/seeders/20210916090516-users.js
+++ api/src/db/seeders/20210916090516-users.js
@@ -20,6 +20,78 @@ module.exports = {
 				role: "SUPER_ADMIN",
 				approved: true,
 				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "test",
+				lastName: "1",
+				email: "test1@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "test",
+				lastName: "2",
+				email: "test2@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "test",
+				lastName: "2",
+				email: "test2@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "test",
+				lastName: "3",
+				email: "test3@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "test",
+				lastName: "4",
+				email: "test4@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "test",
+				lastName: "5",
+				email: "test5@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "Vietphat",
+				lastName: "Group",
+				email: "vietphat@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
+			},
+			{
+				firstName: "Thuan",
+				lastName: "Nguyen",
+				email: "thuan@emolyze.vn",
+				password: "PasswordIsPassword-123",
+				role: "SUPER_ADMIN",
+				approved: true,
+				...require("../utils").timestampGenerator()
 			}
 		]);
 	},
