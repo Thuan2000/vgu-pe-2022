2d19a063388015e76c75e532e8e6528c146826d9
fatwaanugera21 <fatwaanugera21@gmail.com>
2021-09-21 17:42:16 +0800

    Fix ecosystem permission denied

--- frontend/shop/ecosystem.config.js
+++ frontend/shop/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: "shop",
-			script: "sudo bash ../../ci_scripts/install-dependencies/shop.sh && yarn start",
+			script: "sudo bash ../../ci_scripts/install-dependencies/shop.sh && sudo yarn start",
 			max_restarts: 1,
 			kill_timeout: 1600,
 			autorestart: false
