828ffb4fbe7df655b9c0a6e2f3248b6aaa1a1a3d
fatwaanugera21 <fatwaanugera21@gmail.com>
2021-10-22 06:26:10 +0800

    fix database seeding

--- api/src/sequelize/config.js
+++ api/src/sequelize/config.js
@@ -8,11 +8,11 @@ module.exports = {
 		password: process.env.DEV_DB_PASSWORD
 	},
 	production: {
-		dialect: process.env.DEV_DB_DIALECT,
-		host: process.env.DEV_DB_HOST,
-		database: process.env.DEV_DB_DATABASE,
-		port: parseInt(process.env.DEV_DB_PORT),
-		username: process.env.DEV_DB_USERNAME,
-		password: process.env.DEV_DB_PASSWORD
+		dialect: process.env.PROD_DB_DIALECT,
+		host: process.env.PROD_DB_HOST,
+		database: process.env.PROD_DB_DATABASE,
+		port: parseInt(process.env.PROD_DB_PORT),
+		username: process.env.PROD_DB_USERNAME,
+		password: process.env.PROD_DB_PASSWORD
 	}
 };
