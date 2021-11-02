9750547f4e53a0c1ea224ab30e374b3dd4ffb945
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2021-11-02 13:29:53 +0800

    Finish v.1.0

--- frontend/shop/src/components/ui/search.tsx
+++ frontend/shop/src/components/ui/search.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState, useRef } from "react";
 
 import ArrowDownIcon from "@assets/icons/arrow-down-icon";
 import Input from "./storybook/inputs/input";
@@ -10,19 +10,25 @@ import { useTranslation } from "next-i18next";
 import { INameSuggestion } from "@graphql/types.graphql";
 
 const TYPING_TIMEOUT = 350;
+const MAX_SUGGESTIONS = 7;
 
 const Search = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
-  const [getSuggestion, { loading }] = useGetBrNameSuggestionMutation();
+  const [getSuggestion] = useGetBrNameSuggestionMutation();
   const [suggestions, setSuggestions] = useState<INameSuggestion[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
-  const [showSuggestion, setShowSuggestion] = useState(false);
+  const [isShowSuggestion, setIsShowSuggestion] = useState(false);
+  const [focusedSuggestion, setFocusedSuggestion] = useState(-1);
+  const wrapperRef = useRef(null);
   const { t } = useTranslation();
 
   const { pathname, query, ...router } = useRouter();
 
+  /**
+   * Handling suggestions fetch
+   */
   useEffect(() => {
     let timeout: NodeJS.Timeout;
 
@@ -43,6 +49,25 @@ const Search = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputValue]);
 
+  function hideSuggestion() {
+    setIsShowSuggestion(false);
+  }
+  function showSuggestion() {
+    setIsShowSuggestion(true);
+  }
+
+  /**
+   * When input search unfocused
+   */
+  useEffect(() => {
+    document.addEventListener("click", hideSuggestion);
+    return () => document.removeEventListener("click", hideSuggestion);
+  }, [wrapperRef]);
+
+  /**
+   *
+   * @param input Search value
+   */
   function redirectWithParam(input?: string) {
     router.replace({
       pathname,
@@ -57,28 +82,67 @@ const Search = ({
   function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
     setInputValue(e.target.value);
 
-    if (!e.target.value) setShowSuggestion(false);
-    else setShowSuggestion(true);
+    if (!e.target.value) hideSuggestion();
+    else if (!isShowSuggestion) showSuggestion();
   }
 
-  function handleInputClick(sug: string) {
+  function handleSelectedInput(sug: string) {
     searchInputValue(sug.toLowerCase());
     setInputValue(sug);
-    setShowSuggestion(false);
+    hideSuggestion();
   }
 
+  /**
+   * When submitting
+   */
   function handleSearch() {
+    setFocusedSuggestion(-1);
     redirectWithParam();
-    setShowSuggestion(false);
+    hideSuggestion();
+  }
+
+  /**
+   *
+   * @param e All arrow key, enter and such
+   */
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    // Hide suggestions
+    if (e.key === "Escape") hideSuggestion();
+
+    // Selected suggestion
+    if (e.key === "Enter" && focusedSuggestion !== -1)
+      handleSelectedInput(suggestions[focusedSuggestion].name);
+
+    // Selecting suggestion
+    if (e.key === "ArrowDown") {
+      setFocusedSuggestion((old) => (old + 1) % MAX_SUGGESTIONS);
+      if (!isShowSuggestion) showSuggestion();
+    }
+
+    // Selecting suggestion
+    if (e.key === "ArrowUp") {
+      setFocusedSuggestion((old) => Math.max(--old, -1));
+      if (!isShowSuggestion) showSuggestion();
+    }
+  }
+
+  function handleInputFocus() {
+    if (inputValue) showSuggestion();
   }
 
   return (
-    <div className={`relative ${className}`} {...props}>
+    <div
+      onClick={(e) => e.stopPropagation()}
+      ref={wrapperRef}
+      className={`relative ${className}`}
+      {...props}
+    >
       <Form onSubmit={handleSearch}>
         <div className={`flex items-center rounded-md border border-green `}>
           <button
             style={{ height: "45px", width: "166px" }}
             type="button"
+            onClick={hideSuggestion}
             className="flex-center border-r-2"
           >
             <p className="text-paragraph text-dark-blue">{t("Suppliers")}</p>
@@ -88,29 +152,33 @@ const Search = ({
             <Input
               noBorder
               value={inputValue}
-              // onBlur={() => setShowSuggestion(false)}
+              onKeyDown={handleKeyDown}
+              onFocus={handleInputFocus}
               onChange={handleInputChange}
-              className="border-none sm:w-[300px]"
+              className="border-none sm:w-[350px]"
             />
           </div>
           <button className="flex-center border-l-2 sm:w-[45px] sm:h-[45px] border-l-green rounded-sm">
             <SearchIcon />
           </button>
         </div>
-        <div className="bg-white absolute w-full border-l border-r rounded-b-lg overflow-hidden z-[9999]">
-          {showSuggestion &&
-            suggestions.map((sug) => {
+        <div className="bg-white left-[166px] right-0 absolute border-r border-l border-primary rounded-b-lg overflow-hidden z-[9999]">
+          {isShowSuggestion &&
+            suggestions &&
+            suggestions.slice(0, MAX_SUGGESTIONS).map((sug, idx) => {
               return (
                 <p
-                  onClick={() => handleInputClick(sug.name)}
-                  className="py-1 p-1 border-b z-[99999]"
-                  key={sug + "suggestion-key"}
+                  onClick={() => handleSelectedInput(sug.name)}
+                  className={`px-4 py-3 border-b border-primary z-[99999] cursor-pointer 
+                    ${
+                      focusedSuggestion === idx && "bg-gray-10 border-dark-blue"
+                    }
+                  `}
+                  key={sug.highlightedName + "suggestion-key" + idx}
                   dangerouslySetInnerHTML={{
                     __html: sug.highlightedName,
                   }}
-                >
-                  {}
-                </p>
+                />
               );
             })}
         </div>
