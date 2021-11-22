89cb3a0c735c46f03608137978078b3ac61c6773
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2021-11-22 21:58:02 +0800

    Redirection problem

--- frontend/shop/src/pages/index.tsx
+++ frontend/shop/src/pages/index.tsx
@@ -7,10 +7,20 @@ import { generateHeadTitle } from "@utils/seo-utils";
 import { useTranslation } from "react-i18next";
 import UnderDevelopment from "@components/under-development";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { getMeData } from "@utils/auth-utils";
+import { ROUTES } from "@utils/routes";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { locale } = ctx;
-
+  const { company, user } = getMeData();
+  if (!company || !user) {
+    return {
+      redirect: {
+        destination: ROUTES.LOGIN,
+        permanent: false,
+      },
+    };
+  }
   return {
     props: {
       ...(await serverSideTranslations(locale!, ["common"])),
