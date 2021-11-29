92650d8aa3e4cd50d5dfe9777ac2b455e944deef
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2021-11-29 14:50:28 +0800

    Fix no preview

--- api/src/db/seed/sqls/users.sql
+++ api/src/db/seed/sqls/users.sql
@@ -3,3 +3,5 @@ INSERT INTO `users` (`id`, `firstName`, `lastName`, `email`, `password`, `role`,
 (2, "Test", "Owner", "test_owner@gmail.com", "$2b$10$Xlik2Ui8MvmcRV8Bzs4vkuDvReUCPhuCGWt4mBL34FVvKJT8UXR/.", "COMPANY_OWNER", 2),
 (3, "Test", "Staff", "test_staff@gmail.com", "$2b$10$Xlik2Ui8MvmcRV8Bzs4vkuDvReUCPhuCGWt4mBL34FVvKJT8UXR/.", "COMPANY_STAFF", 2),
 (4, "Guest", NULL, "guest@gmail.com", "$2b$10$Xlik2Ui8MvmcRV8Bzs4vkuDvReUCPhuCGWt4mBL34FVvKJT8UXR/.", "GUEST", 2);
+(5, "Thuan", "Nguyen", "thuan@gmail.com", "$2b$10$Xlik2Ui8MvmcRV8Bzs4vkuDvReUCPhuCGWt4mBL34FVvKJT8UXR/.", "SUPER_ADMIN", 1),
+(6, "Chou", "Nguyen", "chou@gmail.com", "$2b$10$Xlik2Ui8MvmcRV8Bzs4vkuDvReUCPhuCGWt4mBL34FVvKJT8UXR/.", "SUPER_ADMIN", 1),
