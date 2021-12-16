3f4dddf2eef2c07cd984ad43d99a8027a8e31167
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2021-12-16 23:01:30 +0800

    Removing product names query

--- api/src/graphql/resolvers/product.resolver.ts
+++ api/src/graphql/resolvers/product.resolver.ts
@@ -1,7 +1,3 @@
 import ProductController from "@controllers/product.controller";
 
 const productController = new ProductController();
-
-export const Query = {
-	productNames: () => productController.getProductNames()
-};
