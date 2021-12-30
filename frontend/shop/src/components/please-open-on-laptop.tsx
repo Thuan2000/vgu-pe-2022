222e316e80ece6bec71371daa93ee08f8261a3b8
KhanhTran <ankhanhct@gmail.com>
2021-12-30 07:27:08 +0700

    Fix responsive

--- frontend/shop/src/components/please-open-on-laptop.tsx
+++ frontend/shop/src/components/please-open-on-laptop.tsx
@@ -18,8 +18,8 @@ const PleaseOpenOnLaptop: React.FC<IPleaseOpenOnLaptopProps> = () => {
             objectFit="cover"
             alt={t("under-development")}
           />
-          <div className="absolute inset-0 flex flex-col justify-end items-center mb-48">
-            <p className="mt-40 px-20 md:px-50 text-3xl font-bold text-center">{t("phone-view-not-ready-text")}</p>
+          <div className="absolute inset-0 flex flex-col justify-end items-center mb-24">
+            <p className="md:w-32 lg:w-48 text-3xl font-bold text-center">{t("phone-view-not-ready-text")}</p>
             <p className="flex justify-center my-2 text-xl z-20 text-center">{t("please-open-on-laptop-text")}</p>
           </div>
         </div>
