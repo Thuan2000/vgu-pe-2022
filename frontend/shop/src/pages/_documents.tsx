b0afda2be24988c98486cdf0a24f3d1a9af94067
Thuan Nguyen <nhthuan20@gmail.com>
2022-01-11 11:18:52 +0700

    fixed error

--- frontend/shop/src/pages/_documents.tsx
+++ frontend/shop/src/pages/_documents.tsx
@@ -1,44 +1,50 @@
 import Document, {
-	Html,
-	Head,
-	Main,
-	NextScript,
-	DocumentContext
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
 } from "next/document";
 import { i18n } from "next-i18next";
-
+import Script from "next/script";
 class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-	render() {
-		const { locale } = this.props.__NEXT_DATA__;
+  render() {
+    const { locale } = this.props.__NEXT_DATA__;
 
-		if (process.env.NODE_ENV !== "production") {
-			i18n!.reloadResources(locale);
-		}
+    if (process.env.NODE_ENV !== "production") {
+      i18n!.reloadResources(locale);
+    }
 
-		return (
-			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-					<script>
-						window.onUsersnapCXLoad = function(api) {
-							api.init();
-						}
-						var script = document.createElement('script');
-						script.defer = 1;
-						script.src = 'https://widget.usersnap.com/global/load/d29420f8-4a66-4ff2-886f-ea64ed0c2d27?onload=onUsersnapCXLoad';
-						document.getElementsByTagName('head')[0].appendChild(script);
-					</script>
-				</body>
-			</Html>
-		);
-	}
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <Script
+            onLoad={() => {
+              // @ts-ignore
+              window.onUsersnapCXLoad = function (api) {
+                api.init();
+              };
+              // @ts-ignore
+              var script = document.createElement("script");
+              // @ts-ignore
+              script.defer = 1;
+              script.src =
+                "https://widget.usersnap.com/global/load/d29420f8-4a66-4ff2-886f-ea64ed0c2d27?onload=onUsersnapCXLoad";
+              document.getElementsByTagName("head")[0].appendChild(script);
+            }}
+          />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
