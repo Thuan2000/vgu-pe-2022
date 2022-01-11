9e488144ce577dfda7a0d7cb15da8b08daea897d
Thuan Nguyen <nhthuan20@gmail.com>
2022-01-11 11:21:46 +0700

    added usersnap scripts

--- frontend/shop/src/pages/_documents.tsx
+++ frontend/shop/src/pages/_documents.tsx
@@ -6,7 +6,6 @@ import Document, {
   DocumentContext,
 } from "next/document";
 import { i18n } from "next-i18next";
-import Script from "next/script";
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -26,21 +25,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script
-            onLoad={() => {
-              // @ts-ignore
-              window.onUsersnapCXLoad = function (api) {
-                api.init();
-              };
-              // @ts-ignore
-              var script = document.createElement("script");
-              // @ts-ignore
-              script.defer = 1;
-              script.src =
-                "https://widget.usersnap.com/global/load/d29420f8-4a66-4ff2-886f-ea64ed0c2d27?onload=onUsersnapCXLoad";
-              document.getElementsByTagName("head")[0].appendChild(script);
-            }}
-          />
         </body>
       </Html>
     );
