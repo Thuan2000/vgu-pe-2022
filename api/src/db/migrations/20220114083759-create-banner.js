cbf91599c1d1a1b986657a6d564475e408ed9bb7
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2022-01-14 16:45:36 +0800

    subscription and partner api

---
+++ api/src/db/migrations/20220114083759-create-banner.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const tableName = "banners";
+
+module.exports = {
+	tableName,
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable(tableName, {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			title: {
+				type: Sequelize.STRING
+			},
+			description: {
+				type: Sequelize.TEXT
+			},
+			imgUrl: {
+				type: Sequelize.STRING
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable(tableName);
+	}
+};
---
+++ frontend/shop/src/components/homepage/bs-constants.tsx
@@ -0,0 +1,42 @@
+export const offerSliderBreakpoints = {
+  320: {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+  580: {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+  1024: {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+  1920: {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+};
+
+// dummy data
+export const data = [
+  {
+    id: 1,
+    title: "banner:promotion-slide-one",
+    imgUrl: "/promotion/banner-1.png",
+  },
+  {
+    id: 2,
+    title: "banner:promotion-slide-two",
+    imgUrl: "/promotion/banner-2.png",
+  },
+  {
+    id: 3,
+    title: "banner:promotion-slide-three",
+    imgUrl: "/promotion/banner-3.png",
+  },
+  {
+    id: 4,
+    title: "banner:promotion-slide-four",
+    imgUrl: "/promotion/banner-4.png",
+  },
+];
