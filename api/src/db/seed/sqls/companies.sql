85224eb32482b3e2b418e0472b7077d5f27ccf4e
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2022-02-07 14:02:01 +0800

    Fix error seed

--- api/src/db/seed/sqls/companies.sql
+++ api/src/db/seed/sqls/companies.sql
@@ -1,6 +1,6 @@
-INSERT INTO `companies` (`id`, `name`, `slug`, `licenseNumber`, `description`, `approved`) VALUES
-(1, "SDConnect", "sdconnect", "FCS345SF", "The biggest company in the world", 1),
-(2, "Test Company", "test", "FCCDS5SF", "Testing company", 1),
-(3, "Thuan ltd", "thuan-ltd", "CSV345SF", "Company owned by Thuan", 1),
-(4, "Chou ltd", "chou-ltd", "NFS345SF", "Company owned by Chou", 1),
-(5, "Fatwa ltd", "fatwa-ltd", "NFS345SF", "Company owned by Fatwa", 1);
+INSERT INTO `companies` (`id`, `name`, `slug`, `licenseNumber`, `description`, `approved`, `ownerId`) VALUES
+(1, "SDConnect", "sdconnect", "FCS345SF", "The biggest company in the world", 1, 1),
+(2, "Test Company", "test", "FCCDS5SF", "Testing company", 1, 2),
+(3, "Thuan ltd", "thuan-ltd", "CSV345SF", "Company owned by Thuan", 1, 3),
+(4, "Chou ltd", "chou-ltd", "NFS345SF", "Company owned by Chou", 1, 4),
+(5, "Fatwa ltd", "fatwa-ltd", "NFS345SF", "Company owned by Fatwa", 1, 5);
