64235744b341f3e8ed60ced6b0b36772beac45a2
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2022-02-21 10:27:14 +0800

    Fix UI some error

--- frontend/shop/src/components/ui/navbar/navbar.tsx
+++ frontend/shop/src/components/ui/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarNavigations from "../navbar-navigations";
 import LanguageSelector from "../language-selector";
 import NavbarBottom from "./navbar-bottom";
@@ -6,13 +6,18 @@ import { ROUTES } from "@utils/routes";
 import Breadcrumb from "../storybook/breadcrumb";
 import { getIsCompanyFullInfo } from "@utils/functions";
 import EditCompanyReminder from "./edit-company-reminder";
+import { isLogin } from "@utils/auth-utils";
 
 interface INavbarProps {
   className: string;
 }
 
 const Navbar: React.FC<INavbarProps> = ({ className }) => {
-  const isFullInfo = getIsCompanyFullInfo();
+  const [isFullInfo, setIsFullInfo] = useState(getIsCompanyFullInfo());
+
+  useEffect(() => {
+    setIsFullInfo(getIsCompanyFullInfo());
+  }, []);
 
   return (
     <div className={`z-50 ${className}`}>
@@ -20,7 +25,7 @@ const Navbar: React.FC<INavbarProps> = ({ className }) => {
         <NavbarNavigations />
         <LanguageSelector />
       </div>
-      {!isFullInfo && <EditCompanyReminder />}
+      {/* {isLogin() && isFullInfo ? <></> : <EditCompanyReminder />} */}
       <div className={`px-10 md:px-48 bg-white`}>
         <NavbarBottom />
         <div className="fic">
