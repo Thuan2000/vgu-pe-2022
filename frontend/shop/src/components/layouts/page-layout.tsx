55038041395b9e4231c2a82369370ac135e6e8dc
Thuan Nguyen <nhthuan20@gmail.com>
2022-02-27 13:42:30 +0700

    more fixes

--- frontend/shop/src/components/layouts/page-layout.tsx
+++ frontend/shop/src/components/layouts/page-layout.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import Navbar from "@components/ui/navbar/navbar";
 import useIsPhone from "src/hooks/isPhone.hook";
 import Footer from "./footer";
+import useIsFullInfoCompChecker from "src/hooks/useIsFullInfoCompChecker";
 
 const PageLayout: React.FC = ({ children }) => {
+  const isFullInfoComp = useIsFullInfoCompChecker();
   const isPhone = useIsPhone();
+
+  const marginTop = isFullInfoComp ? 'mt-36' : 'mt-56';
 
   return (
     <>
       {!isPhone && <Navbar className="fixed top-0 w-full" />}
       {!isPhone && (
-        <div className={`px-10 mb-8 mt-36 md:px-48 relative`}>{children}</div>
+        <div className={`px-10 mb-8 ${marginTop} md:px-48 relative`}>{children}</div>
       )}
       {isPhone && <div>{children}</div>}
       {!isPhone && <Footer />}
