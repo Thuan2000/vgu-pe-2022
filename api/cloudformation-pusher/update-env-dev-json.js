896255d7cd94418bb33ca48dee3ff0032c0ffca7
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2022-04-13 10:10:08 +0800

    Update Docker config

--- api/cloudformation-pusher/update-env-dev-json.js
+++ api/cloudformation-pusher/update-env-dev-json.js
@@ -1,6 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require("fs");
 const refresherParamaterKey = "Refresher";
+
+const dbUsernameParamKey = "DBUsername";
+const dbPassParamKey = "DBPassword";
+const dbNameParamKey = "DBDatabase";
+
+// TODO: Get this from website
+const dbUsernameParamaterVal = "admin";
+const dbPassParamaterVal = "DatabasePassword-123";
+const dbNameParamaterVal = "sdconnect";
+
 const filePath = "env-dev.json";
 
 // read file and make object
@@ -25,6 +35,12 @@ function getUpdatedRefresherValue(c) {
 const updatedContent = content.map(c => {
 	if (c.ParameterKey === refresherParamaterKey) {
 		c.ParameterValue = getUpdatedRefresherValue(c);
+	} else if (c.ParameterKey === dbUsernameParamKey) {
+		c.ParameterValue = dbUsernameParamaterVal;
+	} else if (c.ParameterKey === dbPassParamKey) {
+		c.ParameterValue = dbPassParamaterVal;
+	} else if (c.ParameterKey === dbNameParamKey) {
+		c.ParameterValue = dbNameParamaterVal;
 	}
 
 	return c;
